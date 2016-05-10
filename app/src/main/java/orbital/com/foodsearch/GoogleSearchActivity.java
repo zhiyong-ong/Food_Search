aef2beb2b94c4f6228604487ee59195d89052d1e
zhiyong4113 <zhiyong.ong@hotmail.com>
2016-05-10 11:10:57 +0800

    added google search

---
+++ app/src/main/java/orbital/com/foodsearch/GoogleSearchActivity.java
@@ -0,0 +1,38 @@
+package orbital.com.foodsearch;
+
+import android.app.SearchManager;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class GoogleSearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_google_search);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Button btn = (Button) findViewById(R.id.searchButton);
+        final EditText searchText = (EditText) findViewById(R.id.inputSearch);
+        if(btn != null) {
+            btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
+                    assert searchText != null;
+                    String keyword= searchText.getText().toString();;
+                    intent.putExtra(SearchManager.QUERY, keyword);
+                    startActivity(intent);
+                }
+            });
+        }
+
+    }
+
+}
--- app/src/main/java/orbital/com/foodsearch/MainActivity.java
+++ app/src/main/java/orbital/com/foodsearch/MainActivity.java
@@ -1,7 +1,11 @@
 package orbital.com.foodsearch;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //test
     }
+
+    /** Check if this device has a camera */
+    private boolean checkCameraHardware(Context context) {
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+            // this device has a camera
+            return true;
+        } else {
+            // no camera on this device
+            return false;
+        }
+    }
+
+    public void goSearch(View view) {
+        Intent intent = new Intent(this, GoogleSearchActivity.class);
+        startActivity(intent);
+    }
+
 }
