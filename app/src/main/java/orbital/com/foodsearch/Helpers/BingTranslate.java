767baf2195988f0687ab817d0a5db1e4d979298d
zhiyong4113 <zhiyong.ong@hotmail.com>
2016-07-01 11:59:59 +0800

    Merge branch 'master' of https://github.com/zhiyong4113/Food_Search
    # Conflicts:
    #	app/src/main/java/orbital/com/foodsearch/Activities/OcrActivity.java
    #	app/src/main/java/orbital/com/foodsearch/Helpers/BingSearch.java
    #	app/src/main/java/orbital/com/foodsearch/Models/ImageInsightsPOJO/BestRepresentativeQuery.java
    #	app/src/main/java/orbital/com/foodsearch/Models/ImageInsightsPOJO/Creator.java
    #	app/src/main/java/orbital/com/foodsearch/Models/ImageInsightsPOJO/ImageCaption.java
    #	app/src/main/java/orbital/com/foodsearch/Models/ImageInsightsPOJO/ImageInsightsResponse.java
    #	app/src/main/java/orbital/com/foodsearch/Models/ImageInsightsPOJO/RelatedSearch.java
    #	app/src/main/java/orbital/com/foodsearch/Models/ImageInsightsPOJO/Seller.java
    #	app/src/main/java/orbital/com/foodsearch/Models/ImageInsightsPOJO/ShoppingSources.java
    #	app/src/main/res/layout/card_item.xml

--- app/src/main/java/orbital/com/foodsearch/Helpers/BingTranslate.java
+++ app/src/main/java/orbital/com/foodsearch/Helpers/BingTranslate.java
@@ -1,8 +1,26 @@
 package orbital.com.foodsearch.Helpers;
 
+import com.memetix.mst.language.Language;
+import com.memetix.mst.translate.Translate;
+
 /**
  * Created by zhiyong on 1/7/2016.
  */
 
 public class BingTranslate {
+
+    private static final String LOG_TAG = "FOODIES";
+
+    public static String getTranslatedText(String txt) {
+        Translate.setClientId("foodies");
+        Translate.setClientSecret("yHTnJd2isVkTXxMRAJ7cSDgugHQmwKg+qYT/LbkNrtg=");
+        String translation = null;
+        try {
+            //2nd param is translate to
+            translation = Translate.execute(txt, Language.ENGLISH);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return translation;
+    }
 }
