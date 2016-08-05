5df73654d90189e3ba12f83db689184c2b48f121
zhiyong4113 <zhiyong.ong@hotmail.com>
2016-08-05 14:50:33 +0800

    added multiple lines selection

--- app/src/main/java/orbital/com/foodsearch/Activities/OcrActivity.java
+++ app/src/main/java/orbital/com/foodsearch/Activities/OcrActivity.java
@@ -2,6 +2,7 @@ package orbital.com.foodsearch.Activities;
 
 import android.animation.Animator;
 import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
