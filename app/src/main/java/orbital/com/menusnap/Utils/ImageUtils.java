3f8a38245fc6b033464d8c91edae8910315a703c
abellimz <abellim1309@gmail.com>
2016-08-26 14:09:55 +0800

    Revert to working copy

--- app/src/main/java/orbital/com/menusnap/Utils/ImageUtils.java
+++ app/src/main/java/orbital/com/menusnap/Utils/ImageUtils.java
@@ -12,6 +12,7 @@ import android.graphics.Paint;
 import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Build;
+import android.provider.MediaStore;
 
 import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
